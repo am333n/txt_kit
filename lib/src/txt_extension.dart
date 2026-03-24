903c7f9bcd4b4af7b540a0133a4a096ca3b2c268
Ameen <muhammedameen752@gmail.com>
2026-03-24 16:03:34 +0530

    new extensions

---
+++ lib/src/txt_extension.dart
@@ -0,0 +1,64 @@
+import 'dart:ui';
+
+import 'package:flutter/src/painting/text_style.dart';
+import 'package:txt_kit/txt_kit.dart';
+
+extension TxtStyleExtension on TxtStyle {
+  TxtSpec get spec =>
+      TxtDefaults.specs[this] ??
+      const TxtSpec(fontSize: 14, fontWeight: FontWeight.w400);
+
+  TxtSpec copyWith({
+    double? fontSize,
+    FontWeight? fontWeight,
+    String? fontFamily,
+    double? letterSpacing,
+    double? wordSpacing,
+    double? height,
+    Color? color,
+    TextDecoration? decoration,
+    Color? decorationColor,
+    TextDecorationStyle? decorationStyle,
+    double? decorationThickness,
+    FontStyle? fontStyle,
+    Paint? foreground,
+    Paint? background,
+    List<Shadow>? shadows,
+  }) =>
+      spec.copyWith(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        fontFamily: fontFamily,
+        letterSpacing: letterSpacing,
+        wordSpacing: wordSpacing,
+        height: height,
+        color: color,
+        decoration: decoration,
+        decorationColor: decorationColor,
+        decorationStyle: decorationStyle,
+        decorationThickness: decorationThickness,
+        fontStyle: fontStyle,
+        foreground: foreground,
+        background: background,
+        shadows: shadows,
+      );
+
+  // ── New ──────────────────────────────────────────────────────────────────
+  TextStyle toTextStyle({
+    Color? color,
+    String? fontFamilyFallback,
+    double? fontSize,
+    FontWeight? fontWeight,
+    double? letterSpacing,
+    double? height,
+  }) =>
+      spec
+          .copyWith(
+            color: color,
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            letterSpacing: letterSpacing,
+            height: height,
+          )
+          .toTextStyle(fontFamilyFallback: fontFamilyFallback);
+}
